512e0b21bf3740282584d825cb2a0467ff9ae427
fionaroselegua24-beep <fionaroselegua24@gmail.com>
2025-12-23 17:40:44 +0800

    added get params for end date

--- src/main/java/com/example/attendance/attendance/AttendanceComponents.java
+++ src/main/java/com/example/attendance/attendance/AttendanceComponents.java
@@ -139,6 +139,8 @@ class AttendanceController {
     @GetMapping("/range")
     public List<AttendanceLog> getByDateRange(
         @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime start,
+        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime 
+        
     )
 
 }
