d6a0d7bf3d006ab00170092282ea5f3aefd49adc
fionaroselegua24-beep <fionaroselegua24@gmail.com>
2025-12-23 17:42:02 +0800

    return the data for getbydaterange

--- src/main/java/com/example/attendance/attendance/AttendanceComponents.java
+++ src/main/java/com/example/attendance/attendance/AttendanceComponents.java
@@ -138,8 +138,9 @@ class AttendanceController {
 
     @GetMapping("/range")
     public List<AttendanceLog> getByDateRange(
-        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime start,
-        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime 
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime start,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime 
+        return service.getAttendanceByDateRange(start, end);
         
     )
 
